726ac3407d378b897076775d8dec1b57abac2354
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-09-19 19:23:08 +0200

    Basic working knn

--- src/exercise_01/main.py
+++ src/exercise_01/main.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import numpy as np
+import heapq
+from collections import Counter
 
 DATA_DIR = Path() / "data" / "MNIST"
 TEST_FILEPATH = DATA_DIR / "test.csv"
@@ -21,18 +23,40 @@ def distance(v: np.ndarray, w: np.ndarray) -> float:
         v: np.ndarray(784,)
         w: np.ndarray(784,)
     """
-    return np.sqrt(v.dot(w))
+    # np.linalg.norm(v - w)
+    return np.sqrt(np.sum(np.square(v - w)))
 
 
-def knn(train: np.ndarray, test: np.ndarray, k: int = 1) -> None:
+def get_most_common_label(heap: list) -> int:
+    counter = Counter()
+    for _, label in heap:
+        counter[label] += 1
+    return counter.most_common()[0][0]
+
+
+def knn(train: np.ndarray, test: np.ndarray, k: int = 10) -> None:
+    count = 0
     for row_test in test:
-        min_d = float("inf")
+        test_label = row_test[0]
         label = None
+        k_smallest = []
+        heapq.heapify(k_smallest)
+        min_d = float("inf")
+
         for row_train in train:
             d = distance(row_test[1:], row_train[1:])
-            if d < min_d:
-                min_d, label = d, row_train[0]
-        print(row_test[0], label)
+            min_d = -k_smallest[0][0] if k_smallest else min_d
+            if d < min_d or len(k_smallest) < k:
+                label = row_train[0]
+                if len(k_smallest) < k:
+                    heapq.heappush(k_smallest, (-d, label))
+                else:
+                    heapq.heappushpop(k_smallest, (-d, label))
+        learned_label = get_most_common_label(k_smallest)
+        print(test_label, learned_label)
+        if test_label == learned_label:
+            count += 1
+    print(count)
 
 
 def main():
