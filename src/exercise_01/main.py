0bf50c2a3e72c801382cc81ba9623b8a6b8d1723
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-09-21 12:52:14 +0200

    Fix distance metric and add confusion matrix

--- src/exercise_01/main.py
+++ src/exercise_01/main.py
@@ -8,6 +8,8 @@ DATA_DIR = Path() / "data" / "MNIST"
 TEST_FILEPATH = DATA_DIR / "test.csv"
 TRAIN_FILEPATH = DATA_DIR / "train.csv"
 
+NUM_DIGITS = 10
+
 
 class Distance(str, Enum):
     EUCLIDEAN = "euclidean"
@@ -20,7 +22,6 @@ def read_data() -> tuple[np.ndarray, np.ndarray]:
     return train, test
 
 
-# TODO: add Manhattan
 def distance(
     v: np.ndarray, w: np.ndarray, distance_type: Distance = Distance.EUCLIDEAN
 ) -> float:
@@ -44,20 +45,21 @@ def get_most_common_label(heap: list) -> int:
     counter = Counter()
     for _, label in heap:
         counter[label] += 1
-    return counter.most_common()[0][0]
+    return int(counter.most_common()[0][0])
 
 
 def knn(train: np.ndarray, test: np.ndarray, k: int, distance_type: Distance) -> float:
     correct_count = 0
+    confusion_matrix = np.zeros((NUM_DIGITS, NUM_DIGITS))
     for row_test in test:
-        test_label = row_test[0]
+        test_label = int(row_test[0])
         label = None
         k_smallest = []
         heapq.heapify(k_smallest)
         min_d = float("inf")
 
         for row_train in train:
-            d = distance(row_test[1:], row_train[1:])
+            d = distance(row_test[1:], row_train[1:], distance_type)
             min_d = -k_smallest[0][0] if k_smallest else min_d
             if d < min_d or len(k_smallest) < k:
                 label = row_train[0]
@@ -66,10 +68,11 @@ def knn(train: np.ndarray, test: np.ndarray, k: int, distance_type: Distance) ->
                 else:
                     heapq.heappushpop(k_smallest, (-d, label))
         learned_label = get_most_common_label(k_smallest)
-        # print(test_label, learned_label)
+        confusion_matrix[test_label][learned_label] += 1
         if test_label == learned_label:
             correct_count += 1
     accuracy = correct_count / len(test)
+    print(confusion_matrix)
     return accuracy
 
 
@@ -77,7 +80,7 @@ def main():
     train, test = read_data()
     for k in [1, 3, 5, 10, 15]:
         for distance_type in [Distance.EUCLIDEAN, Distance.MANHATTAN]:
-            accuracy = knn(train, test, k=k, distance_type=Distance.MANHATTAN)
+            accuracy = knn(train, test, k=k, distance_type=distance_type)
             print(k, distance_type, accuracy)
 
 
