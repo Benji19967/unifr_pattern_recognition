40166e6b982213041b57cb96e461d4ad50f5c23d
Benjamin Labrecque <labrecque.benji@gmail.com>
2025-10-01 15:25:01 +0200

    implement kmeans clustering

--- src/distance.py
+++ src/distance.py
@@ -10,9 +10,10 @@ DISTANCE_CACHE = {}
 def distance(
     v: np.ndarray,
     w: np.ndarray,
-    v_idx: int,
-    w_idx: int,
+    v_idx: int | None = None,
+    w_idx: int | None = None,
     distance_type: Distance = Distance.EUCLIDEAN,
+    use_cache: bool = True
 ) -> float:
     """
     Distance metric between the vectors v and w.
@@ -21,9 +22,11 @@ def distance(
         v: np.ndarray(784,)
         w: np.ndarray(784,)
     """
-    _hash = (v_idx, w_idx, distance_type)
-    if _hash in DISTANCE_CACHE:
-        return DISTANCE_CACHE[_hash]
+    if use_cache:
+        assert v_idx is not None and w_idx is not None
+        _hash = (v_idx, w_idx, distance_type)
+        if _hash in DISTANCE_CACHE:
+            return DISTANCE_CACHE[_hash]
 
     distance = None
     match distance_type:
@@ -34,5 +37,6 @@ def distance(
             # np.linalg.norm(v - w, 1)
             distance = np.sum(np.abs(v - w))
 
-    DISTANCE_CACHE[_hash] = distance
+    if use_cache:
+        DISTANCE_CACHE[_hash] = distance
     return distance
--- src/kmeans/main.py
+++ src/kmeans/main.py
@@ -1,9 +1,12 @@
+from collections import defaultdict
 from pathlib import Path
 import numpy as np
 from src.distance import distance
 
 DATA_DIR = Path("data") / "MNIST"
 TRAIN_FILEPATH = DATA_DIR / "train.csv"
+
+MAX_NUM_ITERATIONS = 50
 
 def read_data() -> tuple[np.ndarray, np.ndarray]:
     train = np.genfromtxt(TRAIN_FILEPATH, dtype=int, delimiter=",")
@@ -21,6 +24,19 @@ def calculate_center(points: np.ndarray) -> np.ndarray:
     N = len(points)
     return 1 / N * points.sum(axis=0)
 
+def error_for_cluster(center: np.ndarray, points: np.ndarray) -> float:
+    """
+    How far are the points from the cluster center
+
+    Args:
+        center: (784,)
+        points: (N, 784)
+
+    Returns:
+        float: error
+    """
+    return np.sum((points - center) ** 2)
+
 
 def kmeans(train: np.ndarray, k: int) -> list[list[int]]:
     """
@@ -29,24 +45,44 @@ def kmeans(train: np.ndarray, k: int) -> list[list[int]]:
     # chose k random centers
     num_points = len(train)
     center_indexes = np.random.randint(0, num_points, size=(k,))
+    centers = train[center_indexes]
 
-    clusters = {c: [] for c in center_indexes}
-    for idx, row in enumerate(train):
-        closest_center, min_distance = None, float('inf') 
-        for center_index in center_indexes:
-            d = distance(row, train[center_index], idx, center_index)
-            if d < min_distance:
-                min_distance = d
-                closest_center = center_index
-        clusters[closest_center].append(idx)
+    prev_total_error_for_iteration = 0
+    for _ in range(MAX_NUM_ITERATIONS):
+        total_error_for_iteration = 0
 
-    print(indexes)
+        # assign each point to cluster of closest center
+        clusters = defaultdict(list)
+        for row_idx, row in enumerate(train):
+            closest_center, min_distance = None, float('inf') 
+            for center_idx, center in enumerate(centers):
+                d = distance(row, center, use_cache=False)
+                if d < min_distance:
+                    min_distance = d
+                    closest_center = center_idx
+            clusters[closest_center].append(row_idx)
 
-    pass
+        # compute new cluster centers
+        centers = []
+        for center_idx, cluster_points_indexes in clusters.items():
+            cluster_points_indexes.append(center_idx)
+            cluster_points = train[cluster_points_indexes]
+            cluster_center = calculate_center(cluster_points)
+            centers.append(cluster_center)
+            cluster_error = error_for_cluster(cluster_center, cluster_points)
+            total_error_for_iteration += cluster_error
+
+        if prev_total_error_for_iteration == total_error_for_iteration:
+            break
+
+        prev_total_error_for_iteration = total_error_for_iteration
+
+        print(total_error_for_iteration) 
 
 def main():
     train = read_data()
     for k in [5, 7, 9, 10, 12, 15]:
+        print(k)
         kmeans(train, k)
 
 if __name__ == "__main__":
